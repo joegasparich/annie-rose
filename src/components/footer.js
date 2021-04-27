7cd7f87e2408d1d86b27afd00c859fab6f2c2124
Joe Gasparich <joe.gasparich@accedo.tv>
2021-04-27 13:58:55 +1200

    Make images smaller

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,20 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react"
+import { colours } from "../constants"
 
 import bannerImage from "../images/banner.svg"
 
 const Footer = () => (
     <footer>
+        <p
+            css={{
+                margin: "10px 100px",
+                textAlign: "right",
+                color: colours.red,
+            }}
+        >
+            Website by Joe Gasparich
+        </p>
         <div
             css={{
                 backgroundImage: `url(${bannerImage})`,
